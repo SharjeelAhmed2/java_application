7c5c61546baf299ebb2928a56bc7910adabd4a71
SharjeelAhmed2 <sharjeelahmed614@gmail.com>
2023-05-07 17:38:21 +0500

    first commit, basic packages added

---
+++ src/main/java/com/example/durbur/forInterviewsSake1/ForInterviewsSake1Application.java
@@ -0,0 +1,13 @@
+package com.example.durbur.forInterviewsSake1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForInterviewsSake1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForInterviewsSake1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/durbur/forInterviewsSake1/ForInterviewsSake1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.durbur.forInterviewsSake1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForInterviewsSake1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
